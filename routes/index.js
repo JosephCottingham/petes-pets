542ffcabe49ecd0df9355bc46a06148e0aaa1e03
Joseph Cottingham <joseph.cottingham@protonmail.com>
2021-04-12 12:37:32 -0400

    Implemented responding to JSON

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,12 @@ module.exports = (app) => {
     const page = req.query.page || 1;
 
     Pet.paginate({}, {page: page}).then((results) => {
-      res.render('pets-index', { pets: results.docs, pagesCount: results.pages, currentPage: page });    
+      if (req.header('Content-Type') == 'application/json') {
+        return res.json({ pets: results.docs, pagesCount: results.pages, currentPage: page });
+      // Otherwise we do what we did before
+      } else {
+        res.render('pets-index', { pets: results.docs, pagesCount: results.pages, currentPage: page });    
+      }
     });
   });
 }
